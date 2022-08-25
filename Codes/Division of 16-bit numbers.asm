79392c14ce88e71c1a71bbd8ed7c0c61c0868487
Khalid AlShumayri <53300785+Khalid-AlShumayri@users.noreply.github.com>
2022-08-25 10:41:29 +0300

    Update Division of 16-bit numbers.asm

--- Codes/Division of 16-bit numbers.asm
+++ Codes/Division of 16-bit numbers.asm
@@ -1,1 +1,25 @@
 
+	ORG 00H
+	
+	MOV R2,#255D
+	MOV R3,#220D
+	;First number: 220,255
+	MOV R4,#01D
+	MOV R5,#150d
+	;Second number:150,001
+	
+	
+	;dividing first number by second number
+	;
+	;	220,255/(150,001) = 1000*220/(150,001) + 255/(150,001)
+	;
+	MOV A,R3
+	MOV B,R5
+	DIV AB
+	MOV R6,A
+	MOV R7,B
+	MOV A,R3
+	MOV B,R4
+	DIV AB
+	
+	END
